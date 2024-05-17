bf54031de662adaba6c68b56364faf38ad13f418
joao-fernandes97 <joaofrfernandes.work@gmail.com>
2024-05-17 15:24:49 +0100

    Program stripped

--- GuessTheNumber/Program.cs
+++ GuessTheNumber/Program.cs
@@ -8,42 +8,14 @@ namespace GuessTheNumber
         {
             // Generate a random number
             Random random = new Random();
+            int targetInt = random.Next(1, 101);
 
             // Generate a number between 1 and 100
-            int targetNumber = random.Next(1, 101);
+            Controller controller = new Controller(targetInt);
 
-            int guess;
-            int attempts = 0;
-            bool guessedCorrectly = false;
+            IView view = new ConsoleView(controller, targetInt);
 
-            Console.WriteLine("Welcome to Guess the Number!");
-            Console.WriteLine("I have chosen a number between 1 and 100.");
-
-            // Game loop
-            while (!guessedCorrectly)
-            {
-                Console.Write("Take a guess: ");
-                guess = Convert.ToInt32(Console.ReadLine());
-                attempts++;
-
-                if (guess == targetNumber)
-                {
-                    Console.WriteLine(
-                        "Congratulations! You guessed the number correctly!");
-                    Console.WriteLine("Number of attempts: " + attempts);
-                    guessedCorrectly = true;
-                }
-                else if (guess < targetNumber)
-                {
-                    Console.WriteLine("Too low! Try again.");
-                }
-                else
-                {
-                    Console.WriteLine("Too high! Try again.");
-                }
-            }
-
-            Console.WriteLine("Thank you for playing Guess the Number!");
+            controller.Start();
         }
     }
 }
